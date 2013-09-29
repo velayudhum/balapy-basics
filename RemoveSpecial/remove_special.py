5ee91c3406e404620bac907a0528e359829dc5d9
velayudhum <balakumar.338@gmail.com>
2013-09-29 19:43:24 +0530

    first question is solved

--- RemoveSpecial/remove_special.py
+++ RemoveSpecial/remove_special.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python
 
-print 'Hello World'
+
+sentence = """A pyt#hon i!s a co%nstri^cting s\nake belong@ing to the 
+P*ython (genus), or, more gene[]rally, any sna+ke in the fa-mily #$%
+Py/thoni%dae (cont/aining the Pyt<>?hon g"en\tus)."""
+
+clear_sentence = ''
+
+for letter in sentence:
+	if letter.isalpha() or letter ==' ' or letter ==',' or letter =='.' or letter== ',':
+		clear_sentence+=letter
+print clear_sentence
